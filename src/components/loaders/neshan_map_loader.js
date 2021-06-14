f1e4fcd08113c88a3fc7d73f4a3d6567cac663b8
Seyfollahi <ali.seyfollahi@gmail.com>
2021-06-14 23:36:41 +0430

    fix: fix onLoad event bug for multiple components

--- src/components/loaders/neshan_map_loader.js
+++ src/components/loaders/neshan_map_loader.js
@@ -1,36 +1,44 @@
 const BASE_URL = "https://static.neshan.org";
-const DEFAULT_URL = `${BASE_URL}/sdk/leaflet/1.4.0/leaflet.js`;
+const DEFAULT_URL_SCRIPT = `${BASE_URL}/sdk/leaflet/1.4.0/leaflet.js`;
 const DEFAULT_URL_STYLE = `${BASE_URL}/sdk/leaflet/1.4.0/leaflet.css`;
 
-export default (props) => {
+export default (props => {
   const createScriptStyle = () => {
-    const { onError, onLoad } = props;
+    const {
+      onError,
+      onLoad
+    } = props; //-----import style
 
-    //-----import style
     if (!document.head.querySelector(`link[href="${DEFAULT_URL_STYLE}"]`)) {
       const style = document.createElement("link");
       style.href = DEFAULT_URL_STYLE;
       style.rel = "stylesheet";
       document.head.appendChild(style);
-    }
+    } //------import script
 
-    //------import script
+
     if (window.L) {
       if (onLoad) onLoad();
       return;
     }
-    const script = document.createElement("script");
-    script.src = DEFAULT_URL;
-    script.onload = () => {
+
+    const script = document.body.querySelector(`script[src="${DEFAULT_URL_SCRIPT}"]`) || document.createElement("script");
+
+    script.addEventListener("load", () => {
       if (onLoad) onLoad();
       return;
-    };
-    script.onerror = () => {
+    });
+
+    script.addEventListener("error", () => {
       if (onError) onError();
       return;
-    };
-    document.body.appendChild(script);
+    });
+
+    if(!script.src){
+      script.src = DEFAULT_URL_SCRIPT;
+      document.body.appendChild(script);
+    }  
   };
 
   return createScriptStyle();
-};
+});
