541d8b3acab33cf39a20eb9d7221e71857fff984
Seyfollahi <ali.seyfollahi@gmail.com>
2021-06-14 22:15:29 +0430

    fix: remove imported style

--- dist/loaders/neshan_map_loader.js
+++ dist/loaders/neshan_map_loader.js
@@ -1,36 +1,41 @@
 const BASE_URL = "https://static.neshan.org";
 const DEFAULT_URL = `${BASE_URL}/sdk/leaflet/1.4.0/leaflet.js`;
 const DEFAULT_URL_STYLE = `${BASE_URL}/sdk/leaflet/1.4.0/leaflet.css`;
-
-export default (props) => {
+export default (props => {
   const createScriptStyle = () => {
-    const { onError, onLoad } = props;
+    const {
+      onError,
+      onLoad
+    } = props; //-----import style
 
-    //-----import style
     if (!document.head.querySelector(`link[href="${DEFAULT_URL_STYLE}"]`)) {
       const style = document.createElement("link");
       style.href = DEFAULT_URL_STYLE;
       style.rel = "stylesheet";
       document.head.appendChild(style);
-    }
+    } //------import script
 
-    //------import script
+
     if (window.L) {
       if (onLoad) onLoad();
       return;
     }
+
     const script = document.createElement("script");
     script.src = DEFAULT_URL;
+
     script.onload = () => {
       if (onLoad) onLoad();
       return;
     };
+
     script.onerror = () => {
       if (onError) onError();
       return;
     };
+
     document.body.appendChild(script);
   };
 
   return createScriptStyle();
-};
+});
